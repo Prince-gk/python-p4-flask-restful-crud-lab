46f0bbe18a8b04590017ad6c6669aa5cb5f9a841
Prince-gk <prince.georanja@gmail.com>
2025-04-14 13:13:54 +0300

    Implement CRUD operations for Plant resource and update migration scripts

--- server/migrations/versions/6b7eb36ce0e7_.py
+++ server/migrations/versions/6b7eb36ce0e7_.py
@@ -1,8 +1,8 @@
-"""create table plants
+"""empty message
 
-Revision ID: 1fbfa43fdc29
-Revises: 00528bc9e4f8
-Create Date: 2022-09-26 15:43:25.150032
+Revision ID: 6b7eb36ce0e7
+Revises: 
+Create Date: 2025-04-14 13:12:08.904467
 
 """
 from alembic import op
@@ -10,8 +10,8 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '1fbfa43fdc29'
-down_revision = '00528bc9e4f8'
+revision = '6b7eb36ce0e7'
+down_revision = None
 branch_labels = None
 depends_on = None
 
